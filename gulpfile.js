149962716fdf4a98e18ca356dac8187a0eeb4e92
Spark1er <ricksanford498@gmail.com>
2018-04-27 00:24:41 +0700

    asd

--- gulpfile.js
+++ gulpfile.js
@@ -46,10 +46,10 @@ gulp.task("dev:black-theme-build-index-scss", function() {
 
 gulp.task("watch:index", function() {
   gulp.watch("src/components/**/*.pug", ["dev:build-index-pug"]);
-  gulp.watch("src/components/**/*.scss", ["dev:build-index-scss"]);
   gulp.watch("src/index.pug", ["dev:build-index-pug"]);
   gulp.watch("src/index.scss", ["dev:build-index-scss"]);
   gulp.watch("src/_vars.scss", ["dev:build-index-scss"]);
+  gulp.watch("src/default-theme.scss.scss", ["dev:build-index-scss"]);
 });
 
 /* watch on all pages */
